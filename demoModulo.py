d93ef3693383068cca817f5593d20157d04805aa
Laboratorio 05 Sede SJL <lab05-sjl@certus.edu.pe>
2026-09-10 21:17:26 -0500

    semana 3

---
+++ demoModulo.py
@@ -0,0 +1,3 @@
+import detecciondetemperatura
+test = detecciondetemperatura.detectartemperatura(17,22)
+print(f"El resultado es:{test}")
---
+++ detecciondetemperatura.py
@@ -0,0 +1,12 @@
+#rescribiendo el ejemplo1 con funciones DEF
+#FUNCION RETORNA DATOS 
+def detectartemperatura(lectura,consigna):
+    print(f"temperatura: {lectura}")
+
+    else:
+        #acciones si es falso 
+        print(f"temperatura no es igual a {consigna}")
+        lectura=lectura-1
+        print(f"temperatura:{lectura}")
+
+        
